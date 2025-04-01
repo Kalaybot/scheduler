58f4b25e9e8bd45503438e737ca65b5f45583016
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-03-31 20:22:43 -0400

    Add, Refactor hooks and component to make the app working

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,41 @@
+import { renderHook, act } from "@testing-library/react";
+
+import useVisualMode from "../../hooks/useVisualMode";
+
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND)); // Transition to SECOND
+  expect(result.current.mode).toBe(SECOND); // Assert that the mode is now SECOND
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST); // Should not change if already at initial
+});
+
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND)); // Transition to SECOND
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true)); // Replace SECOND with THIRD
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back()); // Go back to FIRST
+  expect(result.current.mode).toBe(FIRST);
+});
