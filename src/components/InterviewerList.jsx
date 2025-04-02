9374a044328788160275f44ee64146a13202fa58
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-02 14:08:41 -0400

    Install prop-types, refactor InterviewerList to use prop-types and add string in the interviewer props under Form

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,6 +1,7 @@
 import "./InterviewerList.scss";
 
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
@@ -23,5 +24,9 @@ function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default InterviewerList;
