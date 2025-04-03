d3c058a80b09183fcb830a13fea23cb6aeb480aa
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-02 20:05:33 -0400

    Make a mock axios library, refactor Application.test to use this mock library to test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+
+import { fireEvent, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Application from "../Application";
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { queryByText, findByText } = render(<Application />);
+
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
 
 describe("Appointment", ()=> {
