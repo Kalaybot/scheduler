f4c98fff29d585a11cd79acdb3a1f26d3ee2c978
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-04-04 21:17:10 -0400

    Run diferrent test case with same result, intro to cypress testing

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -4,8 +4,17 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get("li").contains("Tuesday").click();
-    cy.contains("li", "Tuesday")
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    // cy.get("li").contains("Tuesday").click();
+    // cy.contains("li", "Tuesday")
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    // cy.contains("li", "Tuesday")
+    //   .click()
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 })
